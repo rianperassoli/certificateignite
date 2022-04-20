4ab8acf362911da75ce1a0dbdafe0cb5e75fe42b
Rian Perassoli <rian.perassoli@gmail.com>
2022-04-20 10:51:23 -0300

    Criada rota para validar se existe certificado para usuario

--- serverless.ts
+++ serverless.ts
@@ -41,6 +41,16 @@ const serverlessConfiguration: AWS = {
         }
       }]
     },
+    verifyCertificate: {
+      handler: "src/functions/verifyCertificate.handler",
+      events: [{
+        http: {
+          path: "verifyCertificate/{id}",
+          method: "get",
+          cors: true
+        }
+      }]
+    },
   },
   package: { individually: true },
   custom: {
