c596ec832c69689fa280915ecb602d2210a81ed4
Rian Perassoli <rian.perassoli@gmail.com>
2022-04-20 09:59:27 -0300

    Passa a criar o certificado no S3

--- serverless.ts
+++ serverless.ts
@@ -21,6 +21,11 @@ const serverlessConfiguration: AWS = {
         Effect: "Allow",
         Action: ["dynamodb:*"],
         Resource: ["*"]
+      },
+      {
+        Effect: "Allow",
+        Action: ["s3:*"],
+        Resource: ["*"]
       }
     ]
   },
@@ -48,6 +53,7 @@ const serverlessConfiguration: AWS = {
       define: { 'require.resolve': undefined },
       platform: 'node',
       concurrency: 10,
+      external: ["chrome-aws-lambda"]
     },
     dynamodb: {
       stages: ["dev", "local"],
--- src/functions/generateCertificate.ts
+++ src/functions/generateCertificate.ts
@@ -3,7 +3,9 @@ import { document } from "../utils/dynamodbClient";
 import { compile } from "handlebars";
 import { join } from "path";
 import { readFileSync } from "fs";
-import * as dayjs from "dayjs";
+import dayjs from "dayjs";
+import chromium from 'chrome-aws-lambda'
+import { S3 } from 'aws-sdk'
 
 interface ICreateCertificate {
   id: string;
@@ -30,13 +32,6 @@ const compileTemplate = async (data: ITemplate) => {
 export const handler: APIGatewayProxyHandler = async (event) => {
   const { id, name, grade } = JSON.parse(event.body) as ICreateCertificate;
 
-  await document.put({
-    TableName: "users_certificate",
-    Item: {
-      id, name, grade, created_at: new Date().getTime()
-    }
-  }).promise()
-
   const response = await document.query({
     TableName: "users_certificate",
     KeyConditionExpression: "id = :id",
@@ -45,7 +40,19 @@ export const handler: APIGatewayProxyHandler = async (event) => {
     }
   }).promise()
 
-  const medalPath  = join(process.cwd(), "src", "templates", "selo.png")
+  const userAlreadyExists = response.Items[0]
+
+  if (!userAlreadyExists) {
+    await document.put({
+      TableName: "users_certificate",
+      Item: {
+        id, name, grade, created_at: new Date().getTime()
+      }
+    }).promise()
+  }
+
+
+  const medalPath = join(process.cwd(), "src", "templates", "selo.png")
   const medal = readFileSync(medalPath, "base64")
 
   const data: ITemplate = {
@@ -53,13 +60,50 @@ export const handler: APIGatewayProxyHandler = async (event) => {
     name,
     grade,
     date: dayjs().format("DD/MM/YYYY"),
-    medal 
+    medal
   }
 
   const content = await compileTemplate(data)
 
+  const browser = await chromium.puppeteer.launch({
+    args: chromium.args,
+    defaultViewport: chromium.defaultViewport,
+    executablePath: await chromium.executablePath,
+  })
+
+  const page = await browser.newPage();
+
+  await page.setContent(content);
+
+  const pdf = await page.pdf({
+    format: "a4",
+    landscape: true,
+    printBackground: true,
+    preferCSSPageSize: true,
+    path: process.env.IS_OFFLINE ? "./certificate.pdf" : null
+  })
+
+  await browser.close()
+
+  const s3 = new S3()
+
+  // await s3.createBucket({
+  //   Bucket: "certificate-ignite-nodejs-rian",
+  // }).promise()
+
+  await s3.putObject({
+    Bucket: "certificate-ignite-nodejs-rian",
+    Key: `${id}.pdf`,
+    ACL: "public-read",
+    Body: pdf,
+    ContentType: "application/pdf"
+  }).promise()
+
   return {
     statusCode: 201,
-    body: JSON.stringify(response.Items[0])
+    body: JSON.stringify({
+      message: "Certificado criado com sucesso",
+      url: `https://certificate-ignite-nodejs-rian.s3.amazonaws.com/${id}.pdf`
+    })
   }
 }
